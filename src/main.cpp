f486b7a1f329167f2a58496af5ea9cd7571486d0
IKACC1 <imrath.keith@g.austincc.edu>
2022-12-09 22:18:28 -0600

    Question 5

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,39 @@
+#include "time.h"
+#include<cstdlib>
+#include "die.h"
+#include "roll.h"
+#include "shooter.h"
+#include "come_out_phase.h"
+#include "point_phase.h"
+#include <memory>
+#include <iostream>
 int main() 
+
 {
-	
+	srand(time(0));
+	Die die1;
+	Die die2;
+	Shooter shooter;
+	Roll* roll;
+	roll=shooter.throw_die(die1,die2);
+	ComeOutPhase come_out_phase;
+	while (come_out_phase.get_outcome(roll)==RollOutcome::natural||come_out_phase.get_outcome(roll)==RollOutcome::craps)
+	{
+		std::cout<<"Rolled: "<<roll->roll_value();
+		std::cout<<"\t Roll Again \n";
+		roll= shooter.throw_die(die1,die2);
+	}
+	std::cout<<"Rolled "<<roll->roll_value()<<" Start of Point Phase\n";
+	std::cout<<"Roll until "<<roll->roll_value()<<" or a 7 is rolled \n";	
+	int point= roll->roll_value();
+	roll = shooter.throw_die(die1,die2);
+	PointPhase point_phase(point);
+	while( point_phase.get_outcome(roll)==RollOutcome::point||point_phase.get_outcome(roll)==RollOutcome::nopoint)//this doesn't make sense, it should only continue while the outcome is nopoint. however this is what the instructions say to do.
+	{
+		std::cout<<roll->roll_value()<<" Roll Again\n";
+		roll= shooter.throw_die(die1,die2);
+	}
+	std::cout<<roll->roll_value()<<" End of Point Phase\n";
+	std::cout<<shooter;
 	return 0;
 }
