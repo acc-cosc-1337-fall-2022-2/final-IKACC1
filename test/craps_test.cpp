79c2d1857a4c6ad1dd12ee3c3b42713e65a9eaa2
IKACC1 <imrath.keith@g.austincc.edu>
2022-12-09 22:00:44 -0600

    Question 1

--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -1,6 +1,29 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "die.h"
+#include "roll.h"
+#include "shooter.h"
+#include "come_out_phase.h"
+#include "point_phase.h"
+#include <memory>
+#include <iostream>
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
+
+TEST_CASE("Test die class") 
+{
+	srand(time(NULL));
+	for(int i =0; i<10;i++)
+		{
+			
+			Die d;
+			d.roll();
+			std::cout<<"\n rolled value: "<<d.rolled_value();
+			REQUIRE((d.rolled_value()>=1));
+			REQUIRE((d.rolled_value()<=6));
+		}
+	
+}
+
